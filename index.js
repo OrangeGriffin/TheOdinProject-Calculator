de4133006b57ffe1288a20362c91bb334d5464b0
OrangeGriffin <76541687+OrangeGriffin@users.noreply.github.com>
2023-04-06 18:56:34 -0500

    Add class to buttons

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const buttons = document.querySelectorAll(".button");
+const display = document.querySelector(".display")
 
 buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    alert(button.id);
+  button.addEventListener("click", (event) => {
+    //alert(button.id);
+    getValue(event);
   });
 });
+
+const getValue = function(event) {
+    let value = Number(event.target.innerText);
+    console.log(value);
+    console.log(typeof value);
+    displayValue(value)
+    return value
+}
+
+const displayValue = function(value) {
+    display.innerText = value;
+}
