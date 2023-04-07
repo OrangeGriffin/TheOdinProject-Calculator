3256e21d276380ece228e3fa9ef5d555b64b746c
OrangeGriffin <76541687+OrangeGriffin@users.noreply.github.com>
2023-04-07 09:22:44 -0500

    Remove logic
    Seemed to be getting too complicated. Removed logic to
    refactor.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const buttons = document.querySelectorAll(".button");
 const display = document.querySelector(".display");
-let enteredValue = '';
+let enteredValue = "";
 let num1 = null;
 let num2 = null;
 let operator = "";
@@ -38,20 +38,5 @@ const displayValue = function (event) {
     value = event.target.innerText;
     enteredValue += value;
     display.innerText = enteredValue;
-  } else if (event.target.classList.contains("operator")) {
-    if (!operator && enteredValue !== null) {
-      num1 = Number(enteredValue);
-      operator = event.target.id;
-      //console.log(operator)
-    } else {
-      num2 = Number(enteredValue);
-      switch (operator) {
-        case "add":
-          let result = add(num1, num2);
-          display.innerText = result;
-          break;
-      }
-    }
-    enteredValue = "";
   }
 };
