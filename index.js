a37bc0d966a95a9bb723a59fe1da08919d11f819
OrangeGriffin <76541687+OrangeGriffin@users.noreply.github.com>
2023-04-07 05:36:48 -0500

    Add outline of functions

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 const buttons = document.querySelectorAll(".button");
 const display = document.querySelector(".display")
 
+const add = function(a,b) {
+
+}
+
+const subtract = function(a,b) {
+
+}
+
+const multiply = function(a,b) {
+
+}
+
+const divide = function(a,b) {
+
+}
+
+const operate = function() {
+
+}
+
 buttons.forEach((button) => {
   button.addEventListener("click", (event) => {
     //alert(button.id);
