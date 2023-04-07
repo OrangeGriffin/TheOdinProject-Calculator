64d6062ee83d4cd50bc4bccd54a274bb28f7b1da
OrangeGriffin <76541687+OrangeGriffin@users.noreply.github.com>
2023-04-07 06:50:06 -0500

    Add working

--- index.js
+++ index.js
@@ -1,39 +1,57 @@
 const buttons = document.querySelectorAll(".button");
 const display = document.querySelector(".display");
-let enteredValue  = '';
-let num1 = 0;
-let num2 = 0;
-let operator = '';
+let enteredValue = '';
+let num1 = null;
+let num2 = null;
+let operator = "";
 
-const add = function(a,b) {
-  return a+b;
-}
+const add = function (a, b) {
+  return a + b;
+};
 
-const subtract = function(a,b) {
-  return a-b;
-}
+const subtract = function (a, b) {
+  return a - b;
+};
 
-const multiply = function(a,b) {
-  return a*b;
-}
+const multiply = function (a, b) {
+  return a * b;
+};
 
-const divide = function(a,b) {
-  return a/b;
-}
+const divide = function (a, b) {
+  return a / b;
+};
 
-const operate = function(num1, num2, operator) {
-
-}
+const operate = function (num1, num2, operator) {};
 
 buttons.forEach((button) => {
   button.addEventListener("click", (event) => {
-    console.log(event.target);
-    let value = event.target.innerText;
-    displayValue(value);
+    //console.log(event.target.classList.contains("decimal"))
+    displayValue(event);
   });
 });
 
-const displayValue = function(value) {
+const displayValue = function (event) {
+  if (
+    event.target.classList.contains("number") ||
+    event.target.classList.contains("decimal")
+  ) {
+    value = event.target.innerText;
     enteredValue += value;
     display.innerText = enteredValue;
-}
+  } else if (event.target.classList.contains("operator")) {
+    if (!operator && enteredValue !== null) {
+      num1 = Number(enteredValue);
+      operator = event.target.id;
+      //console.log(operator)
+    } else {
+      num2 = Number(enteredValue);
+      switch (operator) {
+        case "add":
+          let result = add(num1, num2);
+          display.innerText = result;
+          break;
+      }
+    }
+    enteredValue = "";
+  }
+};
