687a76f15c53edb94e74efcebd7cb0c7f7bc48fb
OrangeGriffin <76541687+OrangeGriffin@users.noreply.github.com>
2023-04-07 09:50:31 -0500

    Refactoring in progress

--- index.js
+++ index.js
@@ -23,20 +23,40 @@ const divide = function (a, b) {
 
 const operate = function (num1, num2, operator) {};
 
-buttons.forEach((button) => {
-  button.addEventListener("click", (event) => {
-    //console.log(event.target.classList.contains("decimal"))
-    displayValue(event);
-  });
-});
+const handleOperands = function (num) {
+  if (!num1) {
+    num1 = num;
+  }
+  console.log("This is handleOperands. Number is: ", num);
+};
 
-const displayValue = function (event) {
+const handleOperator = function (event) {
+  console.log("Went to handleOperator. Which is: ", event.target.innerText);
+  display.innerText = event.target.innerText;
+};
+
+const handleInput = function (event) {
   if (
     event.target.classList.contains("number") ||
     event.target.classList.contains("decimal")
   ) {
-    value = event.target.innerText;
-    enteredValue += value;
-    display.innerText = enteredValue;
+    enteredValue += event.target.innerText;
+    console.log(enteredValue);
+    handleDisplay(enteredValue);
+  } else if (event.target.classList.contains("operator")) {
+    handleOperator(event);
+    num = Number(enteredValue);
+    handleOperands(num);
   }
 };
+
+const handleDisplay = function (enteredValue) {
+  display.innerText = enteredValue;
+};
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    //console.log(event.target.classList.contains("decimal"))
+    handleInput(event);
+  });
+});
